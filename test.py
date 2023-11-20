7fa241475b7aa2d5b6e7c9a1a69df6fc69b1be6c
akchonya <akchonyahokagivna@gmail.com>
2023-11-20 22:17:39 +0200

    edit the prompt

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import g4f
+from icecream import ic
 from random import sample
 
 
 def get_response(prompt):
     response = g4f.ChatCompletion.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "user",
@@ -22,24 +23,26 @@ def get_response(prompt):
     return answer
 
 
-wordlist = "cold (illness), hill, storing, unforgettable, mate, cheerful, decision, significant, impression, personality, reflection, tidy"
+wordlist = "decade, invent, huge, according to, statement, speech"
 
 words = wordlist.split(",")
 shuffled_words = sample(words, len(words))
 mixed_wordlist = ", ".join(shuffled_words)
 
-print(mixed_wordlist)
+
+ic(mixed_wordlist)
+# print()
 
 
 print(f"\n{'='*10}\n")
 
 prompt_sentences = (
-    f"list of words: {mixed_wordlist}\n there is a list of words, some of them may have"
-    " explanations in brackets. you need to create a sentence for each of the words"
-    " with only one gap to fill in. use a1-a2 level vocabulary and grammar. use each"
-    " word once. i expect to see  different sentences with only one gap (that looks"
-    " like ____________) \n expected output formated like '1. (or any other number) SENTENCE HERE. [WORD"
-    " USED HERE]'. answer without any other words except of the words needed. the gaps are the most important part."
+    f"list of words: {mixed_wordlist}\n there is a list of words or collocations, some of them may have"
+    " explanations in brackets. you need to create a sentence for each of the words (collocations)"
+    " with only one gap to fill in. use a1-a2 level vocabulary and grammar. there could be some mistypes - fix them. use each"
+    " word (collocations) once. i expect to see  different sentences with only one gap (that looks"
+    " like ____________) \n expected output formated like '1. (or any other number) SENTENCE HERE. [WORD (collocations)"
+    " USED HERE]'. answer without any other words except of the words (collocations) needed. the gaps are the most important part."
 )
 
 
